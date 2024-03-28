b0c4d61005104322840e1d7dd986562daf941a02
MSkrzypietz <michael.skr97@gmail.com>
2024-03-28 20:33:22 +0100

    Add repl with help and exit commands

---
+++ repl.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	commands := createCommands()
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := strings.ToLower(strings.TrimSpace(scanner.Text()))
+		if input == "" {
+			continue
+		}
+
+		if command, ok := commands[input]; ok {
+			if err := command.callback(); err != nil {
+				fmt.Printf("Error encountered: %s\n", err.Error())
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
